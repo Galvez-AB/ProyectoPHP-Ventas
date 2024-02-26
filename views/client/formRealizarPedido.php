4d2da3eb43da52644b3f8300e2c9a5280ff1e50b
bbluloops <nehisbell.terrones12@gmail.com>
2024-02-26 04:10:22 -0500

    CSS realizarPedido P1

--- views/client/formRealizarPedido.php
+++ views/client/formRealizarPedido.php
@@ -1,123 +1,88 @@
 <?php
 //session_start(); 
-
 class formPagos{
+
     public function formPagoPayPal($totalGeneral){
-        ?>
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-                <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-                <script src="https://www.paypal.com/sdk/js?client-id=ASUkPLxP2DaYls3LtDnqrmNgwobA5eSWMPzE3SnDQ0pkp02fGn2pEGMbgyrmezX-EsJaMgiQf8qWdeXf&currency=USD"></script>
-                <!--  &currency=USD-->
-            </head>
-            <body>
-                <div>
-                    <h1>Detalles de Pago</h1>
-                    <h3>Precio Total: <?php echo $totalGeneral; ?></h3>
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Realizar Pedido</title>
+        <link rel="stylesheet" type="text/css" href="http://localhost/ProyectoDSW/public/css/realizarPedido.css">
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="https://www.paypal.com/sdk/js?client-id=ASUkPLxP2DaYls3LtDnqrmNgwobA5eSWMPzE3SnDQ0pkp02fGn2pEGMbgyrmezX-EsJaMgiQf8qWdeXf&currency=USD"></script>
+    </head>
+    <body>
+        <div class="PanelPedido">
+            <h1>Detalles de Pago</h1>
+            <h3>Precio Total: <?php echo $totalGeneral; ?></h3>
 
-                    <label for="direccion">Dirección de Envío:</label>
-                    <input type="text" id="direccion" name="direccion"><br>
+            <label for="direccion">Dirección de Envío:</label>
+            <input type="text" id="direccion" name="direccion"><br>
+        </div>
 
-                </div>
+        <div class="button-container">
+            <div id="paypal-button-container" class="paypal-buttons-container"></div>
+        </div>
 
-                <div id="paypal-button-container" style="max-width:1000px;"></div>
-
-                <script>
-                    // Desactivar el botón de PayPal inicialmente
-                    document.getElementById('paypal-button-container').style.pointerEvents = 'none';
-
-                    // Escuchar el evento de cambio en el campo de dirección
-                    document.getElementById('direccion').addEventListener('input', function() {
-                        var direccion = this.value.trim(); // Obtener el valor de la dirección
-
-                        // Si la dirección está vacía, desactivar el botón de PayPal; de lo contrario, activarlo
-                        if (direccion === '') {
-                            document.getElementById('paypal-button-container').style.pointerEvents = 'none';
-                        } else {
-                            document.getElementById('paypal-button-container').style.pointerEvents = 'auto';
-                        }
+        <script>
+            paypal.Buttons({
+                style:{
+                    color: 'blue',
+                    shape: 'pill',
+                    label: 'pay'
+                },
+                createOrder: function(data, actions){
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: <?php echo $totalGeneral; ?>
+                            }
+                        }]
                     });
-
-                    paypal.Buttons({
-                        style:{
-                            color: 'blue',
-                            shape: 'pill',
-                            label: 'pay'
-                        },
-                        createOrder: function(data, actions){
-                            return actions.order.create({
-                                purchase_units: [{
-                                    amount: {
-                                        //valor de compra
-                                        value: <?php echo $totalGeneral; ?>
-                                    }
-                                }]
-                            });
-                        },
-                        onCancel: function(data){
+                },
+                onCancel: function(data){
+                    Swal.fire({
+                        icon: "error",
+                        title: "No se realizo el pago",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    console.log(data);
+                },
+                onApprove: function(data, actions){
+                    actions.order.capture().then(function (detalles){
+                        if(detalles.status == 'COMPLETED'){
                             Swal.fire({
-                            icon: "error",
-                            title: "No se realizo el pago",
-                            showConfirmButton: false,
-                            timer: 1500
-                            });
-                            console.log(data);
-                        },
-                        onApprove: function(data, actions){
-                            actions.order.capture().then(function (detalles){
-                                
-                                if(detalles.status == 'COMPLETED'){
-                                    Swal.fire({
-                                        title: "¡Excelente!",
-                                        text: "El pago se ha realizado correctamente",
-                                        icon: "success",
-                                        confirmButtonText: "Aceptar"
-                                        }).then((result) => {
-                                        if (result.isConfirmed) {
-                                            window.location.href = "http://localhost/ProyectoDSW/controllers/client/controlPago.php";
-                                        }
-                                    });
+                                title: "¡Excelente!",
+                                text: "El pago se ha realizado correctamente",
+                                icon: "success",
+                                confirmButtonText: "Aceptar"
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.href = "http://localhost/ProyectoDSW/controllers/client/controlPago.php";
                                 }
-                                //console.log(detalles);//VER LOS DETALLES DE LA COMPRA
-                            });  
+                            });
                         }
-                        
-                    }).render('#paypal-button-container');
-                </script>
+                    });  
+                }
+                
+            }).render('#paypal-button-container');
+        </script>
 
-            </body>
-            </html>
-        <?php
+    </body>
+    </html>
+
+    <?php
     }
-
-    public function formDetallesCompra($totalGeneral){
-        ?>
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
-            </head>
-            <body>
-                <h1>Detalles de Pago</h1>
-                <p>Precio Total: <?php echo $totalGeneral; ?></p>
-
-                <label for="direccion">Dirección de Envío:</label>
-                <input type="text" id="direccion" name="direccion"><br>
-
-            </body>
-            </html>
-        <?php
-    }
-
 
 }        
 ?>
+
+
+
 
 
 
